15c6378221fd4e7a9c1b6abf5928e95d934d08fa
Diego R. Lima <contact@diegoribeiro.net>
2019-01-30 23:06:19 -0300

    Creation of the models from "Post" domain

---
+++ app/Domains/Post/Models/Post.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Domains\Post\Models;
+
+use App\Support\Domains\Model;
+use App\Domains\Account\Models\User;
+use App\Domains\Upload\Models\Upload;
+
+class Post extends Model
+{
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'slug', 'content'
+    ];
+
+    /**
+     * The categories that the post belongs to.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function categories()
+    {
+        return $this->belongsToMany(Category::class, 'post_category');
+    }
+
+    /**
+     * Get the author that owns the post.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function author()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the post's thumbnail.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\MorphOne
+     */
+    public function thumbnail()
+    {
+        return $this->morphOne(Upload::class, 'owner');
+    }
+
+    /**
+     * Define the default resource class.
+     *
+     * @return string
+     */
+    public static function defaultResource(): string
+    {
+        return \App\Domains\Post\Resources\PostResource::class;
+    }
+
+    /**
+     * Define the default resource collection class.
+     *
+     * @return string|null
+     */
+    public static function defaultResourceCollection(): ?string
+    {
+        return \App\Domains\Post\Resources\PostResourceCollection::class;
+    }
+}
